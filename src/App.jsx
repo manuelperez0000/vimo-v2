77a826fe646288f069c8279742224c21c4d89aa1
manuel perez <manuelperez.0000@gmail.com>
2024-01-14 15:08:45 -0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* import { startRecording } from "grabador" */
+
+
+function App() {
+  return (
+    <div>
+      <button > Start </button>
+      <button > Stop </button>
+    </div>
+  )
+}
+
+export default App
